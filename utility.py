f362eb99bb28eab3c7f78a3ea1d7f480c94b4165
dhirajnair <dhiraj.nair.in@gmail.com>
2024-05-31 16:51:41 +0530

    updated utilities with code and using gpt-4o as the model now and temperature to 0

--- utility.py
+++ utility.py
@@ -1,6 +1,6 @@
 import os
 import base64
-from langchain.openai  import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers.string import StrOutputParser
 
 def format_data_for_openai(diffs, readme_content, commit_messages):
